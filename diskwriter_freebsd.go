7b64c8499fffcb038c84e1ca082d6bba48121d09
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2022-11-17 14:41:32 +0200

    Update go.mod

--- diskwriter_freebsd.go
+++ diskwriter_freebsd.go
@@ -4,8 +4,6 @@
 package fsutil
 
 import (
-	"os"
-
 	"github.com/tonistiigi/fsutil/types"
 	"golang.org/x/sys/unix"
 )
@@ -16,11 +14,4 @@ func createSpecialFile(path string, mode uint32, stat *types.Stat) error {
 
 func mkdev(major int64, minor int64) uint64 {
 	return unix.Mkdev(uint32(major), uint32(minor))
-}
-
-func renameFile(src, dst string) error {
-	if err := os.Rename(src, dst); err != nil {
-		return errors.Wrapf(err, "failed to rename %s to %s", src, dst)
-	}
-	return nil
 }
