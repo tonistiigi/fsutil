8176e09d439732a91160bdc41f1d5be5898346c0
coryb <cbennett@netflix.com>
2023-06-28 12:32:30 -0700

    handle mkdir race for diskwriter
    If using the diskwriter hanlders in parallel we might see a common
    directory being created in parallel, only one will win.  This allows us
    to gracefully retry/re-evaluate when we get a syscall.EEXIST error on
    the mkdir instead of returning an error.

--- diskwriter.go
+++ diskwriter.go
@@ -162,6 +162,10 @@ func (dw *DiskWriter) HandleChange(kind ChangeKind, p string, fi os.FileInfo, er
 	switch {
 	case fi.IsDir():
 		if err := os.Mkdir(newPath, fi.Mode()); err != nil {
+			if errors.Is(err, syscall.EEXIST) {
+				// we saw a race to create this directory, so try again
+				return dw.HandleChange(kind, p, fi, nil)
+			}
 			return errors.Wrapf(err, "failed to create dir %s", newPath)
 		}
 		dw.dirModTimes[destPath] = statCopy.ModTime
