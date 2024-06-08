4f64c4498264693afafae6cecbcb6589653cce59
Kirill A. Korinsky <kirill@korins.ky>
2024-06-08 11:00:32 +0100

    Avoid using syscall for errors on unix
    syscall is deprecated and non updated library whcih should be replaced
    by new one with name x/sys.
    
    Anyway, it may work on some platform and some errors migth be missed on
    some. A good example is OpenBSD which has lack of syscall.EBADMSG on
    amd64 and 386 but has on arm64.
    
    Here I moved all errors to two files: one for unix which uses sys.unix,
    and one for everything else which uses syscall as fallback.

--- stat_unix.go
+++ stat_unix.go
@@ -15,7 +15,7 @@ import (
 func loadXattr(origpath string, stat *types.Stat) error {
 	xattrs, err := sysx.LListxattr(origpath)
 	if err != nil {
-		if errors.Is(err, syscall.ENOTSUP) {
+		if errors.Is(err, ENOTSUP) {
 			return nil
 		}
 		return errors.Wrapf(err, "failed to xattr %s", origpath)
