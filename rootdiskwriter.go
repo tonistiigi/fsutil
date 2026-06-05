c0fdb93a8f833b16716ee0dafbb031bd66c4e33f
Tonis Tiigi <tonistiigi@gmail.com>
2026-06-04 17:01:16 -0700

    root: enable RootDiskWriter tests on Windows
    Add Windows root timestamp handling that matches the existing DiskWriter
    metadata behavior by skipping symlink timestamps. Split RootDiskWriter
    metadata rewriting by platform so Windows avoids unsupported ownership
    and mode updates, then include RootDiskWriter in the Windows test matrix.
    
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- rootdiskwriter.go
+++ rootdiskwriter.go
@@ -292,28 +292,6 @@ func rootChtimes(root Root, p string, un int64) error {
 	return nil
 }
 
-func rewriteRootMetadata(root Root, p string, stat *types.Stat) error {
-	for key, value := range stat.Xattrs {
-		root.LSetxattr(p, key, value, 0)
-	}
-
-	if err := root.Lchown(p, int(stat.Uid), int(stat.Gid)); err != nil {
-		return errors.WithStack(err)
-	}
-
-	if os.FileMode(stat.Mode)&os.ModeSymlink != 0 {
-		return root.LChtimes(p, time.Unix(0, stat.ModTime))
-	}
-	if err := root.Chmod(p, os.FileMode(stat.Mode)); err != nil {
-		return errors.WithStack(err)
-	}
-	if err := rootChtimes(root, p, stat.ModTime); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 type rootLazyFileWriter struct {
 	lease    *rootLease
 	f        *os.File
